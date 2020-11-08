95a7eb125fbd667dc361ad28e50694743302d151
Joe <jostheblessing@gmail.com>
2020-11-08 10:42:40 +0200

    bg(cors issue): fix cors issue
     - fix cors issue

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import cors from 'cors';
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 
@@ -9,6 +10,8 @@ import requestsRouter from './routes/requests'
 
 dotenv.config()
 const app = express()
+app.use(cors());
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
