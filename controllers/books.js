fdb6ca7908f419e2a92ff4b19421e8005a59537b
Joe <jostheblessing@gmail.com>
2020-11-13 21:23:49 +0200

    fix request issues

--- controllers/books.js
+++ controllers/books.js
@@ -7,49 +7,49 @@ import multer from '../middleware/multer'
 
 const addBook = async (req, res) => {
     try {
-        const uploadImage = multer.single('image')
+        // const uploadImage = multer.single('image')
 
-        uploadImage(req, res, async (err) => {
-            if (err) {
-                // A Multer error occurred when uploading.
-                return res.status(400).json({
-                    status: 400,
-                    error: err.message
-                })
-            } 
-            // Everything went fine.
-            const uploader = async (path) => await uploads(path, 'Images')
+        // uploadImage(req, res, async (err) => {
+        //     if (err) {
+        //         // A Multer error occurred when uploading.
+        //         return res.status(400).json({
+        //             status: 400,
+        //             error: err.message
+        //         })
+        //     } 
+        //     // Everything went fine.
+        //     const uploader = async (path) => await uploads(path, 'Images')
 
-            const { path } = req.file
-            const newPath = await uploader(path)
-            const url = newPath
-            fs.unlinkSync(path)
+        //     const { path } = req.file
+        //     const newPath = await uploader(path)
+        //     const url = newPath
+        //     fs.unlinkSync(path)
 
-            const { name, author } = req.body
-            const { userId } = req.user
+        const { name, author, image } = req.body
+        const { userId } = req.user
 
-            const { valid, errors } = validateBook(name, author)
-            if(!valid) {
-                return res.status(400).json({
-                    status: 400,
-                    error: errors
-                })
-            }
-            console.log('Image url', url)
-            const book = new Book({
-                name,
-                author,
-                image: url,
-                userId,
-                createdAt: new Date().toISOString()
-            })
-
-            const newBook = await book.save()
-            return res.status(201).json({
-                status: 201,
-                data: newBook
-            })
+        //     const { valid, errors } = validateBook(name, author)
+        //     if(!valid) {
+        //         return res.status(400).json({
+        //             status: 400,
+        //             error: errors
+        //         })
+        //     }
+        //     console.log('Image url', url)
+        const book = new Book({
+            name,
+            author,
+            image,
+            userId,
+            createdAt: new Date().toISOString()
         })
+
+        const newBook = await book.save()
+        return res.status(201).json({
+            status: 201,
+            data: newBook
+        })
+        // })
     }catch (err) {
         res.status(500).json({
             status: 500,
